546aa80f86f495995cca00f6687d754e25cf3d3f
satancucumber <amatomato@yandex.ru>
2025-03-16 22:10:23 +0300

    fix lab1_3

--- lab1/lab1_3.cpp
+++ lab1/lab1_3.cpp
@@ -2,17 +2,25 @@
 #include "mpi.h"
 #include <iostream>
 
-int main(int argc, char **argv)
-{
-    int rank, size;   
-    
+int main(int argc, char **argv) {
+    int rank, size;
+
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    for (int i = 1; i <= 10; ++i) {
-       std:: cout << rank+1 << " x " << i << " = " <<(rank+1) * i << std::endl;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    // Обходим все числа от 1 до 10
+    for (int number = 1; number <= 10; ++number) {
+        // Проверяем, должен ли текущий процесс обработать это число
+        if ((number - 1) % size == rank) {
+            // Выводим таблицу умножения для данного числа
+            for (int i = 1; i <= 10; ++i) {
+                std::cout << number << " x " << i << " = " << (number * i) << std::endl;
+            }
+        }
     }
+
     MPI_Finalize();
- 
-  return 0;
+
+    return 0;
 }
