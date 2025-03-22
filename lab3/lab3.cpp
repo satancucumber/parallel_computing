20f81a10b9d9a64aa618e71369ec571125ca4852
satancucumber <amatomato@yandex.ru>
2025-03-22 21:03:34 +0300

    fix lab3

--- lab3/lab3.cpp
+++ lab3/lab3.cpp
@@ -4,8 +4,8 @@
 #include <ctime>
 #include "mpi.h"
 
-#define ROWS 5
-#define COLUMNS 6
+#define ROWS 5       // Количество строк в матрице
+#define COLUMNS 6    // Количество столбцов в матрице
 
 int main(int argc, char **argv) {
     int rank, size;
@@ -13,13 +13,19 @@ int main(int argc, char **argv) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    int matrix[ROWS][COLUMNS];
-    int modified_matrix[ROWS][COLUMNS];
+    // Локальные данные для каждого процесса
+    int *local_matrix = nullptr;    // Исходные данные процесса
+    int *local_modified = nullptr;  // Модифицированные данные процесса
+    int local_rows = 0;             // Количество строк, обрабатываемых процессом
 
-    // Генерация исходной матрицы (0 и 1) на процессе 0
+    // Этап 1: Генерация и распределение данных
     if (rank == 0) {
+        // Только корневой процесс создает полную матрицу
+        int matrix[ROWS][COLUMNS];
         srand(time(NULL));
         std::cout << "Исходная матрица:\n";
+        
+        // Заполнение матрицы случайными значениями 0/1
         for (int i = 0; i < ROWS; i++) {
             for (int j = 0; j < COLUMNS; j++) {
                 matrix[i][j] = rand() % 2;
@@ -27,79 +33,157 @@ int main(int argc, char **argv) {
             }
             std::cout << "\n";
         }
-    }
 
-    // Рассылка исходной матрицы от процесса 0 всем остальным
-    if (rank == 0) {
-        for (int dest = 1; dest < size; dest++) {
+        // Распределение строк между процессами
+        for (int dest = 0; dest < size; dest++) {
+            // Подсчет строк для текущего процесса-получателя
+            int num_rows = 0;
             for (int i = 0; i < ROWS; i++) {
-                MPI_Send(matrix[i], COLUMNS, MPI_INT, dest, 0, MPI_COMM_WORLD);
+                if (i % size == dest) num_rows++;
             }
-        }
-    } else {
-        for (int i = 0; i < ROWS; i++) {
-            MPI_Recv(matrix[i], COLUMNS, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        }
-    }
-
-    // Инициализация modified_matrix
-    for (int i = 0; i < ROWS; i++) {
-        for (int j = 0; j < COLUMNS; j++) {
-            modified_matrix[i][j] = matrix[i][j];
-        }
-    }
-
-    // Обработка нечетных строк
-    for (int i = 0; i < ROWS; i++) {
-        if (i % size == rank) {  // Распределение строк между процессами
-            if (i % 2 == 1) {   // Инвертирование нечетных строк
-                for (int j = 0; j < COLUMNS; j++) {
-                    modified_matrix[i][j] = 1 - modified_matrix[i][j];
+            
+            // Упаковка данных для отправки
+            int *send_buffer = new int[num_rows * COLUMNS];
+            int idx = 0;
+            for (int i = 0; i < ROWS; i++) {
+                if (i % size == dest) {
+                    for (int j = 0; j < COLUMNS; j++) {
+                        send_buffer[idx * COLUMNS + j] = matrix[i][j];
+                    }
+                    idx++;
                 }
             }
-        }
-    }
-
-    // Сбор модифицированных строк на процессе 0
-    if (rank == 0) {
-        for (int i = 0; i < ROWS; i++) {
-            int source = i % size;
-            if (source != 0) {
-                MPI_Recv(modified_matrix[i], COLUMNS, MPI_INT, source, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            
+            // Отправка данных процессам (или сохранение для себя)
+            if (dest == 0) {
+                local_rows = num_rows;
+                local_matrix = new int[local_rows * COLUMNS];
+                std::copy(send_buffer, send_buffer + num_rows * COLUMNS, local_matrix);
+            } else {
+                MPI_Send(&num_rows, 1, MPI_INT, dest, 0, MPI_COMM_WORLD);
+                MPI_Send(send_buffer, num_rows * COLUMNS, MPI_INT, dest, 0, MPI_COMM_WORLD);
             }
+            delete[] send_buffer;
         }
     } else {
-        for (int i = 0; i < ROWS; i++) {
-            if (i % size == rank) {
-                MPI_Send(modified_matrix[i], COLUMNS, MPI_INT, 0, 1, MPI_COMM_WORLD);
+        // Получение данных от корневого процесса
+        MPI_Recv(&local_rows, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        local_matrix = new int[local_rows * COLUMNS];
+        MPI_Recv(local_matrix, local_rows * COLUMNS, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    }
+
+    // Этап 2: Обработка данных
+    // Инициализация модифицированной матрицы
+    local_modified = new int[local_rows * COLUMNS];
+    std::copy(local_matrix, local_matrix + local_rows * COLUMNS, local_modified);
+
+    // Инвертирование нечетных строк
+    for (int local_i = 0; local_i < local_rows; local_i++) {
+        // Вычисление глобального индекса строки
+        int global_i = rank + local_i * size;
+        if (global_i % 2 == 1) {  // Если строка нечетная
+            for (int j = 0; j < COLUMNS; j++) {
+                local_modified[local_i * COLUMNS + j] = 1 - local_modified[local_i * COLUMNS + j];
             }
         }
     }
 
-    // Рассылка обновленной матрицы всем процессам
+    // Этап 3: Сбор результатов на корневом процессе
     if (rank == 0) {
-        for (int dest = 1; dest < size; dest++) {
+        int modified_matrix[ROWS][COLUMNS];  // Полная модифицированная матрица
+        
+        // Инициализация матрицы данными корневого процесса
+        for (int i = 0; i < ROWS; i++) {
+            for (int j = 0; j < COLUMNS; j++) {
+                modified_matrix[i][j] = (i % size == 0) ? local_matrix[(i / size) * COLUMNS + j] : 0;
+            }
+        }
+
+        // Заполнение данных корневого процесса
+        int idx = 0;
+        for (int i = 0; i < ROWS; i++) {
+            if (i % size == 0) {
+                for (int j = 0; j < COLUMNS; j++) {
+                    modified_matrix[i][j] = local_modified[idx * COLUMNS + j];
+                }
+                idx++;
+            }
+        }
+
+        // Получение данных от других процессов
+        for (int src = 1; src < size; src++) {
+            int num_rows;
+            MPI_Recv(&num_rows, 1, MPI_INT, src, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            int *recv_buffer = new int[num_rows * COLUMNS];
+            MPI_Recv(recv_buffer, num_rows * COLUMNS, MPI_INT, src, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+            // Распаковка полученных данных
+            idx = 0;
             for (int i = 0; i < ROWS; i++) {
-                MPI_Send(modified_matrix[i], COLUMNS, MPI_INT, dest, 2, MPI_COMM_WORLD);
+                if (i % size == src) {
+                    for (int j = 0; j < COLUMNS; j++) {
+                        modified_matrix[i][j] = recv_buffer[idx * COLUMNS + j];
+                    }
+                    idx++;
+                }
             }
+            delete[] recv_buffer;
+        }
+
+        // Этап 4: Рассылка обновленной матрицы всем процессам
+        for (int dest = 0; dest < size; dest++) {
+            int num_rows = 0;
+            for (int i = 0; i < ROWS; i++) {
+                if (i % size == dest) num_rows++;
+            }
+            
+            // Упаковка данных для рассылки
+            int *send_buffer = new int[num_rows * COLUMNS];
+            int idx = 0;
+            for (int i = 0; i < ROWS; i++) {
+                if (i % size == dest) {
+                    for (int j = 0; j < COLUMNS; j++) {
+                        send_buffer[idx * COLUMNS + j] = modified_matrix[i][j];
+                    }
+                    idx++;
+                }
+            }
+            
+            // Рассылка данных
+            if (dest == 0) {
+                std::copy(send_buffer, send_buffer + num_rows * COLUMNS, local_modified);
+            } else {
+                MPI_Send(send_buffer, num_rows * COLUMNS, MPI_INT, dest, 2, MPI_COMM_WORLD);
+            }
+            delete[] send_buffer;
+        }
+
+        // Вывод результатов
+        std::cout << "\nМодифицированная матрица:\n";
+        for (int i = 0; i < ROWS; i++) {
+            for (int j = 0; j < COLUMNS; j++) {
+                std::cout << modified_matrix[i][j] << " ";
+            }
+            std::cout << "\n";
         }
     } else {
-        for (int i = 0; i < ROWS; i++) {
-            MPI_Recv(modified_matrix[i], COLUMNS, MPI_INT, 0, 2, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        }
+        // Отправка данных корневому процессу
+        MPI_Send(&local_rows, 1, MPI_INT, 0, 1, MPI_COMM_WORLD);
+        MPI_Send(local_modified, local_rows * COLUMNS, MPI_INT, 0, 1, MPI_COMM_WORLD);
+
+        // Получение обновленной матрицы
+        MPI_Recv(local_modified, local_rows * COLUMNS, MPI_INT, 0, 2, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     }
 
-    // Подсчет отрицательных элементов
+    // Этап 5: Подсчет отрицательных элементов
     int local_count = 0;
-    for (int i = rank; i < ROWS; i += size) {
-        for (int j = 0; j < COLUMNS; j++) {
-            if (modified_matrix[i][j] < 0) {
-                local_count++;
-            }
+    for (int i = 0; i < local_rows * COLUMNS; i++) {
+        if (local_modified[i] < 0) {
+            local_count++;
         }
     }
 
-    // Сбор результатов на процессе 0
+    // Сбор статистики на корневом процессе
     int global_count = 0;
     if (rank == 0) {
         global_count = local_count;
@@ -109,19 +193,14 @@ int main(int argc, char **argv) {
             global_count += temp;
         }
         double average = static_cast<double>(global_count) / ROWS;
-        
-        std::cout << "\nМодифицированная матрица:\n";
-        for (int i = 0; i < ROWS; i++) {
-            for (int j = 0; j < COLUMNS; j++) {
-                std::cout << modified_matrix[i][j] << " ";
-            }
-            std::cout << "\n";
-        }
-        std::cout << "\nСреднее количество отрицательных элементов: " 
-                  << average << std::endl;
+        std::cout << "\nСреднее количество отрицательных элементов: " << average << std::endl;
     } else {
         MPI_Send(&local_count, 1, MPI_INT, 0, 3, MPI_COMM_WORLD);
     }
+
+    // Освобождение ресурсов
+    delete[] local_matrix;
+    delete[] local_modified;
 
     MPI_Finalize();
     return 0;
